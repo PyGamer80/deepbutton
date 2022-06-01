312ec739d1871dd3cee13599c7289988f6e16da1
pygamer80 <pygamer80@gmail.com>
2022-06-01 02:17:13 -0300

    Checkbox

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState}from 'react'
+import {TouchableHighlight, TextInput, SafeAreaView, Text, View, Alert } from 'react-native'
+import { Checkbox } from 'react-native-paper';
+import Estilos from './componentes/estilos/estilos.js'
 
 export default function App() {
+  
+  const [checked,setChecked]=useState(false)
+  const [valor,setValor]=useState('N1')
+  const [valor2,setValor2]=useState('N2')
+  const [resultado,setResultado]=useState('Result')
+  
+  const soma=()=>{
+
+  
+    if(parseInt(valor) >= parseInt(valor2)){
+      return(
+        setResultado(parseInt(valor)))}
+        else{
+          setResultado(parseInt(valor2))
+        }
+
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={Estilos.container}>
+      
+      
+      <Checkbox
+     status={checked ? 'checked':'unchecked'}
+      onPress={() => {
+        setChecked(!checked)
+      }}
+    />
+
       <StatusBar style="auto" />
-    </View>
+
+      <Text>msg</Text>
+      <TextInput 
+      style={Estilos.entradatexto}
+      value={String(valor)}
+      onChangeText={(texto)=>{setValor(texto)}}
+      placeholder="useless placeholder"
+      /> 
+            
+      <TextInput 
+      style={Estilos.entradatexto}
+      value={String(valor2)}
+      onChangeText={(texto2)=>{setValor2(texto2)}}
+     
+      />
+      
+      <TextInput 
+      style={Estilos.entradatexto}
+      value={String(resultado)}
+      onChangeText={(texto)=>{setResultado(texto)}}
+      /> 
+      <TouchableHighlight
+      style={Estilos.btn}
+      onPress={()=>soma()}
+      >
+        <Text>Soma</Text>
+      </TouchableHighlight>
+     
+     
+    
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ componentes/estilos/estilos.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native"
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      entradatexto:{
+          marginVertical:10,
+          width:'80%',
+          height:50,
+          borderWidth:3,
+          borderRadius:20,
+          backgroundColor:'#aaa'
+      },
+      btn:{
+        marginVertical:10,
+        width:'80%',
+        height:50,
+        borderWidth:3,
+        borderRadius:20,
+        backgroundColor:'#aaa'
+      }
+})
